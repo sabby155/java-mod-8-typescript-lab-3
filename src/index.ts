59caac28395b1a65fec4a9bb2389ba8fd6b3e694
sabby155 <nebula155@gmail.com>
2022-08-15 20:38:06 -0400

    ts lab 3 complete

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,24 @@
 // your code here
+
+const button = document.getElementById("btn") as HTMLButtonElement;
+const input1 = document.getElementById("firstInput") as HTMLInputElement;
+const input2 = document.getElementById("secondInput") as HTMLInputElement;
+
+button!.addEventListener("click", function () {
+    console.log(prepend(input1!.value, input2!.value));
+});
+
+function prepend(inputString: string, numSpaces: number | string) {
+    // your code here
+    let space = "";
+
+    for (let i = 0; i < numSpaces; i++) {
+        //Putting underscore vs sace so you can see it in output
+        space += " ";
+    }
+    return `${space}${inputString}`;
+}
+
+
+
+console.log(prepend("happy", 10));
